a27c871286d65b280c81d787951ae42b49603ae1
Redayoub-lang <redayoub9@gmail.com>
2024-02-26 23:52:35 -0800

    2-binary_tree_insert_right.c

--- binary_trees.h
+++ binary_trees.h
@@ -29,7 +29,6 @@ typedef struct binary_tree_s heap_t;
 
 /**
  * struct levelorder_queue_s - Level order traversal queue.
- * @node: A node of a binary tree.
  * @next: The next node to traverse to in the binary tree.
  */
 typedef struct levelorder_queue_s
