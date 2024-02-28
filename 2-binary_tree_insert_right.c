471c92bbafe1cf0ce6e7cc7be7c6e55e8d1f0ceb
Redayoub-lang <redayoub9@gmail.com>
2024-02-28 08:35:34 -0800

    3-binary_tree_delete.c

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -6,6 +6,9 @@
  * @parent: A pointer to the node to insert the right-child in.
  * @value: The value to store in the new node.
  *
+ * Return: If parent is NULL or an error occurs - NULL.
+ *         Otherwise - a pointer to the new node.
+ *
  * Description: If parent already has a right-child, the new node
  *              takes its place and the old right-child is set as
  *              the right-child of the new node.
